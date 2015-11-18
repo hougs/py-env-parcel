5545b8ef822712ebc57999a2eb42a46e1a85240b
Juliet Hougland <juliet@cloudera.com>
2015-11-18 15:55:49 -0800

    Preinstalled anaconda.

--- make-parcel.sh
+++ make-parcel.sh
@@ -11,16 +11,20 @@ LINUX_PLATFORM=el6
 
 # Url to get Anaconda distribution from.
 BASE_CONDA_URL=http://repo.continuum.io/archive/.nogpl/
-
 CONDA_URL=${BASE_CONDA_URL}AnacondaNoGPL-${VERSION}-${PLATFORM}.sh
 SRC_DIR=src
 TARGET_DIR=ANACONDA-$VERSION
 
+# Create directory to work in.
 mkdir target
-
-cp -r src target/$TARGET_DIR
+cp -r template target/$TARGET_DIR
 cd target
 
 curl -o $TARGET_DIR/AnacondaNoGPL-${VERSION}-${PLATFORM}.sh $CONDA_URL
+
+mkdir $TARGET_DIR/XXX
+chmod +x $TARGET_DIR/AnacondaNoGPL-${VERSION}-${PLATFORM}.sh
+$TARGET_DIR/AnacondaNoGPL-${VERSION}-${PLATFORM}.sh -p 
+
 tar cvvzf ${TARGET_DIR}.tar.gz $TARGET_DIR
 mv ${TARGET_DIR}.tar.gz ${TARGET_DIR}-${LINUX_PLATFORM}.parcel
